6252e5fe21e273764f3e71043cbbf210da102c14
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2022-12-30 10:10:00 +0530

    Create bubbleSort.cpp

---
+++ bubbleSort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            if(arr[j]>arr[j+1])
+             swap(arr[j], arr[j+1]);
+        }
+    }
+}
+
+int main() {
+    int arr[5];
+    for(int i=0; i<5; i++){
+        cin >> arr[i];
+    }
+    
+    bubbleSort(arr,5);
+    for(int i=0; i<5; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
