18d38f8a2a54a01c849bd14090b3b551d1168db1
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-08 13:11:29 +0530

    Create 2DarrayDeclarInFunc.cpp

---
+++ 2DarrayDeclarInFunc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[][4], int m, int n){
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cout << arr[i][j] << " ";
+        }cout << endl;
+    }
+}
+
+int main() {
+    int arr[3][4];
+    for(int i=0; i<3; i++){
+        for(int j=0; j<4; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    display(arr,3,4);
+
+    return 0;
+}
