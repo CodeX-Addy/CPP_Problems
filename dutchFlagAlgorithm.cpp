f6d28fd5eafbd86356aed16f3702d9989d299e9a
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-04 14:26:02 +0530

    Create dutchFlagAlgorithm.cpp

---
+++ dutchFlagAlgorithm.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   int s=0,e=n-1,mid=0;
+   while(mid<=e){
+      if(arr[mid]==0){
+         arr[mid] = arr[s];
+         arr[s] = 0;
+         s++;
+         mid++;
+      }
+      else if(arr[mid]==1)
+      mid++;
+      else if(arr[mid]==2){
+         arr[mid] = arr[e];
+         arr[e] = 2;
+         e--;
+      }
+   }
+}
