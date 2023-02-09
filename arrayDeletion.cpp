e8d9ada48aeeb9216d72cc29d9160d372e060f2f
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-09 21:13:33 +0530

    Create arrayDeletion.cpp

---
+++ arrayDeletion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void display(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void deleteElement(int arr[], int size, int index){
+    for(int i=index; i<size-1; i++){
+        arr[i] = arr[i+1];
+    }
+    
+}
+int main() {
+    int arr[5] = {1,2,3,4,5},size=5;
+    display(arr,size);
+    deleteElement(arr,size,2);
+    size = size - 1;
+    display(arr,size);
+    
+
+    return 0;
+}
