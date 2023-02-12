469f7a9b2a2be7ea38d2bc78d11fd466767d8bae
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-12 20:56:24 +0530

    Create playingWithPointers.cpp

---
+++ playingWithPointers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+    int *p = &num;
+    
+    //cout << *p;
+    cout << &num << endl;
+    cout << p << endl;
+    cout << num << endl;
+    cout << *p << endl;
+    
+    cout << sizeof(num) << endl;
+    cout << sizeof(p) << endl;
+    int *p = &num;
+    *p = (*p)*10;
+    cout << num << endl;
+    cout << *p << endl;
+    int * p = NULL;
+    cout << "The value of above pointer is " << *p;
+    
+    return 0;
+}
