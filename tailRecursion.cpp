c54720f3d674a84ca67762ef05f771a977bd2b45
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-24 18:56:38 +0530

    Update tailRecursion.cpp

--- tailRecursion.cpp
+++ tailRecursion.cpp
@@ -11,3 +11,15 @@ int main(){
     printN(10);
 }
 // Output- 10 9 8 7 6 5 4 3 2 1 0
+
+// For increasing order
+void printN(int n){
+    if(n>10)
+     return;
+    cout << n << " "; 
+    printN(n+1);
+}
+int main(){
+    printN(0);
+}
+// Outpu:- 0 1 2 3 4 5 6 7 8 9 10
