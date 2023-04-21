9563c4fe3666225b248ef827effe246e5aa0b283
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-04-21 22:23:53 +0530

    Update binarytoDecimal.cpp

--- binarytoDecimal.cpp
+++ binarytoDecimal.cpp
@@ -8,7 +8,7 @@ int main() {
     int ans = 0;
     int i=0;
     while(n!=0){
-        int digit = n%10;
+        int digit = n % 10;
         if(digit==1){
             ans = ans + pow(2,i);
         }
