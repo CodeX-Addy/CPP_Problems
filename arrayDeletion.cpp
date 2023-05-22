7fb78538677f46849c4899ee3d96b34eeea842c1
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-22 21:14:45 +0530

    Update arrayDeletion.cpp

--- arrayDeletion.cpp
+++ arrayDeletion.cpp
@@ -9,7 +9,7 @@ void display(int arr[], int n){
 
 void deleteElement(int arr[], int size, int index){
     for(int i=index; i<size-1; i++){
-        arr[i] = arr[i+1]
+        arr[i] = arr[i+1];
     }
     
 }
