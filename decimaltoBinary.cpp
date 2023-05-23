26049ef2f911383f5a8269d8507f3b158cb079c9
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-23 20:32:48 +0530

    Update decimaltoBinary.cpp

--- decimaltoBinary.cpp
+++ decimaltoBinary.cpp
@@ -9,7 +9,7 @@ int main() {
     int i=0;
     while(n!=0){
         int bit = n&1;
-        ans = (bit*pow(10,i)) + ans;
+        ans = (bit*pow(10,i)) + ans
         n = n>>1;
         i++;
     }
