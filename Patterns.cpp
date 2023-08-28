62232238b1883664415d399d3a6ae334ac6a699c
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-08-29 01:27:29 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -136,3 +136,26 @@ int main() {
 
     return 0;
 }
+
+// Online C++ compiler to run C++ program online
+// 1
+// 12
+// 123
+// 1234
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int cnt = 1;
+    for(int i=0; i<n;++i){
+        for(int j=0; j<i+1; ++j){
+            cout << j+1;
+            
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
