23afd50d8acf6d190487482567a3fac7db889821
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-08-29 23:18:17 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -159,3 +159,34 @@ int main() {
 
     return 0;
 }
+
+//Hollow square or retangle using for loop
+// Online C++ compiler to run C++ program online
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    
+    for(int i=0; i<n; i++){
+        
+        for(int j=0; j<m; j++){
+            if(i==0 || i==n-1){
+            cout << "* ";
+            }
+            else{
+                if(j==0 || j==m-1){
+                cout << "* ";
+            }
+            else{
+                cout << "  ";
+            }
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
