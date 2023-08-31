6c1921113dbe0eeee714e0de56f90d6b96f7c4a5
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-08-31 21:18:17 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -188,3 +188,27 @@ int main() {
 
     return 0;
 }
+
+//Full pyramid pattern
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int r=0; r<n; r++){
+       
+        for(int c=0; c<n-r-1; c++){
+            cout << " ";
+        }
+        
+        for(int c=0; c<r+1; c++){
+            cout << "* ";
+        }
+        cout << endl;
+        
+    }
+
+    return 0;
+}
