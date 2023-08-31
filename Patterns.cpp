fba8ef99b60498ce3a74630dcf217d7164c6b575
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-08-31 21:39:55 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -212,3 +212,29 @@ int main() {
 
     return 0;
 }
+
+//Diamond pattern
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    int n=num/2;
+    
+    for(int r=0; r<n; r++){
+       
+        for(int c=0; c<n-r-1; c++){
+            cout << " ";
+        }
+        
+        for(int c=0; c<r+1; c++){
+            cout << "* ";
+        }
+        cout << endl;
+        
+    }
+
+    return 0;
+}
