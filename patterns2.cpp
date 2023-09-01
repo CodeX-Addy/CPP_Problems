97275022cd2e5334766379079e735b5a434dc45c
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-01 15:08:33 +0530

    Create patterns2.cpp

---
+++ patterns2.cpp
@@ -0,0 +1,34 @@
+//Solid half diamond
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+
+
+  for(int row=0; row<n; row++){
+    for(int col=0; col<row+1; col++){
+      cout << "* ";
+    }
+    cout << endl;
+
+  }
+  for(int i=0; i<n; i++){
+      for(int j=0; j<n-i-1; j++){
+        cout << "* ";
+      }
+      cout << endl;
+    }
+}
