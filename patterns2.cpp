65cff14c4dea33d331fccaad931e064970e9f23e
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-01 21:40:18 +0530

    Update patterns2.cpp

--- patterns2.cpp
+++ patterns2.cpp
@@ -93,3 +93,44 @@ int main(){
     cout << endl;
   }
 }
+
+//Butterfly pattern
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        for(int k=0; k<2*n-2*i-1; k++){
+        cout << " ";
+      }
+      for(int j=0; j<i+1; j++){
+          cout << "*";
+      }
+      cout << endl;
+    
+     }
+     for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            cout << "*";
+        }
+        for(int k=0; k<2*i+1; k++){
+        cout << " ";
+      }
+      for(int j=0; j<n-i; j++){
+          cout << "*";
+      }
+      cout << endl;
+    
+      
+    }
+    
+
+    return 0;
+}
