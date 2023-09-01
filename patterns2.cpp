3d76a1bfe5e83fbe673ecbf497b826200ecf803b
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-01 15:10:09 +0530

    Update patterns2.cpp

--- patterns2.cpp
+++ patterns2.cpp
@@ -32,3 +32,43 @@ int main(){
       cout << endl;
     }
 }
+
+//Hollow inverted pyramid
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  for(int i=0; i<n; i++){
+    for(int j=0; j<i; j++){
+      cout << " ";
+    }
+    for(int j=0; j<n-i; j++){
+      if(j==0 || j==n-i-1){
+        cout << "* "; 
+      }
+      else{
+        cout << "  ";
+      }
+    }
+    cout << endl;
+  }
+
+  // for(int row=0; row<n; row++){
+  //   for(int col=0; col<row+1; col++){
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+
+  // }
+  // for(int i=0; i<n; i++){
+  //     for(int j=0; j<n-i-1; j++){
+  //       cout << "* ";
+  //     }
+  //     cout << endl;
+  //   }
+
+  
+  
+}
