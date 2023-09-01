ee6c82c8d77b6cafd3b11460cea0b2de913e6b5b
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-02 00:35:01 +0530

    Update patterns2.cpp

--- patterns2.cpp
+++ patterns2.cpp
@@ -139,7 +139,30 @@ int main() {
     
       
     }
-    
+    return 0;
+}
+
+//A fancy pattern
+// Online C++ compiler to run C++ program online
+
+//fancy pattern
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<2*i+1; j++){
+            if(j%2 == 1){
+                cout << "*";
+            }
+            else{
+                cout << i+1;
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
