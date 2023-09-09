9438036e48c4104b282f268222a5c75b02137e69
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-09 17:28:31 +0530

    Update patterns2.cpp

--- patterns2.cpp
+++ patterns2.cpp
@@ -208,3 +208,30 @@ int main(){
     }
     return 0;
 }
+
+//Numeric hollow half pyramid
+// 1
+// 12
+// 1 3
+// 1  4
+// 12345
+
+#include<iostream>
+using namespace std;
+
+int main(){
+        int n;
+        cin >> n;
+        
+        for(int i=1; i<=n; ++i){
+                for(int j=1; j<=i; j++){
+                        if(j==1 || j==i || i==n){
+                                cout << j;
+                        }
+                        else{
+                                cout << " ";
+                        }
+                }
+                cout << endl;
+        }
+}
