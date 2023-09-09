bf89e484c813faca1d8c529bf5ab11040c23bc81
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-09 17:05:40 +0530

    Update patterns2.cpp

--- patterns2.cpp
+++ patterns2.cpp
@@ -168,3 +168,43 @@ int main() {
 
     return 0;
 }
+
+
+//Numeric palindrome equilateral pyramid
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+#include<iostream>
+using namespace std;
+//Numeric palindrome equilateral triangle
+int main(){
+    int n;
+    cin >> n;
+    int k=n;
+    for(int i=0; i<n; ++i){
+        int c=1;
+        for(int j=0; j<k; j++){
+            if(j<n-i-1){
+                cout << " ";
+            }
+            else if(j<=n-1){
+                cout << c;
+                c++;
+            }
+            else if(j==n){
+                c = c-2;
+                cout << c;
+                c--;
+            }
+            else{
+                cout << c;
+                c--;
+            }
+        }
+        k++;
+        cout << endl;
+    }
+    return 0;
+}
