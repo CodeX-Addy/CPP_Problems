c7168be3ad0eb8944d2ef28fd3945062e3cd7be1
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-14 20:02:01 +0530

    Update occurence.cpp

--- occurence.cpp
+++ occurence.cpp
@@ -64,3 +64,64 @@ int main() {
     cout << ans << endl;
     return 0;
 }
+
+//write a program to find the total occurence of an element
+#include <iostream>
+using namespace std;
+
+int firstOccur(int arr[], int n, int target){
+    int s=0;
+    int e=n-1;
+    int ans = -1;
+    while(s<=e){
+        int mid = s + (e-s)/2;
+        if(arr[mid] == target){
+            ans = mid;
+            e = mid-1;
+        }
+        else if(arr[mid] < target){
+            s = mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+    }
+    return ans;
+}
+
+int lastOccur(int arr[], int n, int target){
+    int s=0;
+    int e=n-1;
+    int ans = -1;
+    while(s<=e){
+        int mid = s + (e-s)/2;
+        if(arr[mid] == target){
+            ans = mid;
+            s = mid+1;
+        }
+        else if(arr[mid] < target){
+            s = mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+    }
+    return ans;
+}
+
+int totalOccur(int arr[], int n, int target){
+        int first = firstOccur(arr, n, target);
+        int last = lastOccur(arr, n, target);
+        int sol = last-first + 1;
+        return sol;
+}
+
+int main() {
+
+    int arr[] = {10,20,30,30,30,40,40,40,40,50};
+    int n = sizeof(arr)/sizeof(int);
+    
+    int ans = totalOccur(arr, n, 30);
+    cout << ans << endl;
+    return 0;
+}
