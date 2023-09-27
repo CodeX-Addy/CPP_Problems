0ccc1b6e28860f69d175bbdc4158acd169a096e0
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-28 01:24:31 +0530

    Update vectors.cpp

--- vectors.cpp
+++ vectors.cpp
@@ -102,6 +102,8 @@ int main(){
   arr.push_back(v3);
   arr.push_back(v4);
   arr.push_back(v5);
+
+  //Printing the vector
   for(int i=0; i<arr.size(); ++i){
     for(int j=0; j<arr[i].size(); ++j){
       cout << arr[i][j] << " ";
