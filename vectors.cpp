014c2b9f7255fb9725542f2e00b420b21190796f
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-09-28 01:23:50 +0530

    Update vectors.cpp

--- vectors.cpp
+++ vectors.cpp
@@ -89,6 +89,27 @@ int main(){
   }
 }
 //Jagged vector
+int main(){
+ vector<vector<int> > arr(5, vector<int>(5,8));
+  vector<int> v1(5,0);
+  vector<int> v2(6,0);
+  vector<int> v3(7,0);
+  vector<int> v4(8,0);
+  vector<int> v5(9,0);
+
+  arr.push_back(v1);
+  arr.push_back(v2);
+  arr.push_back(v3);
+  arr.push_back(v4);
+  arr.push_back(v5);
+  for(int i=0; i<arr.size(); ++i){
+    for(int j=0; j<arr[i].size(); ++j){
+      cout << arr[i][j] << " ";
+    }
+    cout << endl;
+  }
+  
+ }
 
 
 
