9287e490e7f50d86f6385848f954d13fe60b8eb2
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-10-02 09:31:03 +0530

    Update firstRepeating.cpp

--- firstRepeating.cpp
+++ firstRepeating.cpp
@@ -2,15 +2,15 @@
 #include<climits>
 using namespace std;
 // O(n2)
-// int firstRepeat(int arr[], int n){
-//     for(int i=0; i<n; ++i){
-//         bool isRep = false;
-//         for(int j=i+1; j<n; ++j){
-//             if(arr[i] == arr[j]){
-//                 bool isRep = true;
-//                 return i+1;
-//             }
-//         }
-//     }
-//     return -1;
-// }
+int firstRepeat(int arr[], int n){
+    for(int i=0; i<n; ++i){
+        bool isRep = false;
+        for(int j=i+1; j<n; ++j){
+            if(arr[i] == arr[j]){
+                bool isRep = true;
+                return i+1;
+            }
+        }
+    }
+    return -1;
+}
