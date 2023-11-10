53e782f7a3ac80fdcbc838856c635ee54fd38bd9
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-11-10 20:05:22 +0530

    string reversal

--- reverseString.cpp
+++ reverseString.cpp
@@ -28,3 +28,23 @@ int main() {
 
     return 0;
 }
+
+//String reversal using stack
+#include <iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+    string str = "can u help me out..";
+    stack<char> s;
+    for(int i=0; i<str.length(); ++i){
+        char ch = str[i];
+        s.push(ch);
+    }
+    
+    while(!s.empty()){
+        cout << s.top();
+        s.pop();
+    }cout << endl;
+    return 0;
+}
