d7a6cc7dd0d6d37758e674ec87d8dc3df8417ce2
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-17 21:11:36 +0530

    add: .

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -401,3 +401,39 @@ int main() {
     
     return 0;
 }
+
+// Pattern
+/*
+
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1 
+
+*/
+
+// Approach 1: With extra variable
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int i = 1;
+    
+    while(i <= n){
+        int j = 1;
+        int val = i;
+        while(j <= i){
+            cout << val << " ";
+            j += 1;
+            val -= 1;
+        }cout << endl;
+        i += 1;
+    }
+    
+    return 0;
+}
