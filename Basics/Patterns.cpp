b7226949d584701e715b1ae84bd37d4ee15a93fc
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-17 22:56:06 +0530

    add: practice

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -795,3 +795,37 @@ int main() {
     
     return 0;
 }
+
+//Pattern
+/*
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int i = 0;
+    
+    while(i < n){
+        int j = 0;
+        while(j < n-i){
+            cout << "* ";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+    
+    return 0;
+}
