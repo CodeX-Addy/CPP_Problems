4ab0a8b9f6009c42843b5cee7cb8581799671adb
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-17 20:37:51 +0530

    add: practice
    Added a program to print a number pattern based on user input.

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -272,3 +272,37 @@ int main() {
 
     return 0;
 }
+
+
+// Pattern
+/*
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int i = 0;
+    
+    while(i < n){
+        int j = 0;
+        while(j <= i){
+            cout << i + 1 << " ";
+            j += 1;
+        }cout << endl;
+        i += 1;
+    }
+    
+    return 0;
+}
