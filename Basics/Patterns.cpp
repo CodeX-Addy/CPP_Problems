8a2d40fd2d7c2399041ec773089f0f643e42a950
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-17 19:00:44 +0530

    add: practice

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -1,4 +1,5 @@
 // Various patterns related questions using while loop
+
 // Problem - 01
 *****
 *****
@@ -234,6 +235,39 @@ int main() {
         }
         cout << endl;
         
+    }
+
+    return 0;
+}
+
+// Pattern
+/*
+
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int i = 0;
+    
+    while(i < n){
+        int j = 0;
+        while(j < n){
+            cout << j + 1 << " ";
+            j = j + 1;
+        }cout << endl;
+        i = i + 1;
     }
 
     return 0;
