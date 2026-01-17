8dcd33408def0d0e96e23bc6f5fd3a9cabaa8d47
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-17 21:12:48 +0530

    add: practice

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -437,3 +437,27 @@ int main() {
     
     return 0;
 }
+
+//Approach 2: Without extra variable
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int i = 1;
+    
+    while(i <= n){
+        int j = 1;
+        while(j <= i){
+            cout << i - j + 1 << " ";
+            j += 1;
+        }cout << endl;
+        i += 1;
+    }
+    
+    return 0;
+}
