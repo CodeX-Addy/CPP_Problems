b6a1d8e00663a8710527d9bf15a6f665adeb30c6
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-17 21:21:54 +0530

    add: practice

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -497,3 +497,37 @@ int main() {
     
     return 0;
 }
+
+// Pattern
+/*
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int i = 1;
+    
+    char c = 'A';
+    
+    while(i <= n){
+        int j = 1;
+        while(j <= n){
+            cout << char(c + j - 1) << " ";
+            j += 1;
+        }cout << endl;
+        i += 1;
+    }
+    
+    return 0;
+}
