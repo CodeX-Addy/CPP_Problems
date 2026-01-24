56f9ce2dc0de60aad0a50e87ece9babe37f9b5df
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-01-24 23:00:16 +0530

    add: digits
    Added functionality to print the reverse number from digits.

--- Basics/Digits.cpp
+++ Basics/Digits.cpp
@@ -1,4 +1,4 @@
-// Make a number from the digits
+// Make number from the digits
 #include<iostream>
 using namespace std;
 
@@ -16,5 +16,30 @@ int main(){
         ans = ans * 10 + digit;
         n = n / 10;
     }
+    cout << ans << endl;
+}
+
+// Print reverse number from digits
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    
+    int n;
+    int ans = 0;
+    
+    int i = 0;
+    while(true){
+        cin >> n;
+        if(n == -1)
+            break;
+        
+        int digit = n % 10;
+        ans += digit * pow(10, i);
+        n = n / 10;
+        i++;
+    }
+    
     cout << ans << endl;
 }
